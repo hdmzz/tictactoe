a3ef492e9ebf1c32f53642d89223976cff0f3d1e
unknown <hdamitzian@gmail.com>
2021-05-29 00:02:46 +0200

    debut jeu et gstion des tours x et circle

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-const cellElements = document.querySelectorAll('[data-cell]')
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const xClass = 'x';
+const circleClass = 'circle';
+let circleTurn
+
+startGame()
+
+function startGame() {
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, {once: true})
+    });
+    setBoardHoverClass()
+}
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? circleClass : xClass
+    placeMark(cell, currentClass)
+    //changement de tour 
+    swapTurns()
+    setBoardHoverClass()
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(xClass)
+    board.classList.remove(circleClass)
+    if (circleTurn) {
+        board.classList.add(circleClass)
+    } else {
+        board.classList.add(xClass)
+    }
+}
