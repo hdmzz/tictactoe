f47db1a54e373032ed858ecaf773addb6d07451a
unknown <hdamitzian@gmail.com>
2021-05-29 14:46:37 +0200

    winning combination

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
 const xClass = 'x';
 const circleClass = 'circle';
+const winningCombinations = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    
+]
 let circleTurn
 
 startGame()
